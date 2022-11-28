44dd7f2e570711f252e10fdb79c450c660247971
Yuxiao Guo <yuxgu@microsoft.com>
2022-11-28 20:19:09 +0800

    Fix issue

--- tools/split_dataset.py
+++ tools/split_dataset.py
@@ -9,7 +9,7 @@ import argparse
 
 
 def split_dataset_with_ratio(src_folder: str, dst_folder:str, num_pieces: int, kept_pieces: int, \
-    seed=666, scopes=['train']):
+    seed=666, scopes=None):
     """_summary_
 
     Args:
@@ -19,22 +19,24 @@ def split_dataset_with_ratio(src_folder: str, dst_folder:str, num_pieces: int, k
         kept_pieces (int): the number of pieces to be kept after divided
         seed (int, optional): the random seed to shuffle the list. Defaults to 666.
     """
+    if scopes is None:
+        scopes = ['train']
     random.seed(seed)
     phases = os.listdir(src_folder)
 
     for phase in phases:
         src_phase_path = os.path.join(src_folder, phase)
-        samples = sorted(os.listdir(samples))
+        samples = sorted(os.listdir(src_phase_path))
         random.shuffle(samples)
-        p_pieces = num_pieces if phase in scopes else 1
-        k_pieces = kept_pieces if phase in scopes else 1
-        for p_idx in range(p_pieces):
-            if p_idx > k_pieces:
+        for p_idx in range(num_pieces):
+            if p_idx > kept_pieces:
                 continue
             dst_phase_path = os.path.join(f'{dst_folder}_p{num_pieces}s{p_idx}', phase)
             os.makedirs(dst_phase_path, exist_ok=True)
-            for s_idx in range(p_idx, len(samples), p_pieces):
-                shutil.move(os.path.join(src_phase_path, samples[s_idx]), \
+            p_pieces = num_pieces if phase in scopes else 1
+            s_pieces = p_idx if phase in scopes else 0
+            for s_idx in range(s_pieces, len(samples), p_pieces):
+                shutil.copy(os.path.join(src_phase_path, samples[s_idx]), \
                     os.path.join(dst_phase_path, samples[s_idx]))
 
 def parse_arguments():
